aa8e828eeb0aed00a585dba10797e5c9a7ceabc5
H3rN4n <desouza030689@gmail.com>
2015-03-17 13:47:17 -0300

    filter updated

--- public/modules/imageuploaders/filters/cloudinary.client.filter.js
+++ public/modules/imageuploaders/filters/cloudinary.client.filter.js
@@ -6,7 +6,7 @@ angular.module('imageuploaders').filter('cloudinaryProfile', [
 			// Cloudinary directive logic
 			// ...
 			var res = input.split("/upload/");
-			input = res[0]+ '/upload/w_150,h_150,c_thumb,g_face/' + res[1];
+			input = res[0]+ '/upload/w_150,h_150,c_thumb/' + res[1];
 			return input;
 		};
 	}
