7a161fd969000793c01c2594e19d8642050d1d12
H3rN4n <desouza030689@gmail.com>
2015-03-23 22:44:59 -0300

    faqs

--- public/modules/faqs/config/faqs.client.config.js
+++ public/modules/faqs/config/faqs.client.config.js
@@ -4,8 +4,8 @@
 angular.module('faqs').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('sidebar', 'Faqs', 'faqs', 'dropdown', '/faqs(/create)?', true, null, null, 'icon-info');
-		Menus.addSubMenuItem('sidebar', 'faqs', 'List Faqs', 'faqs');
-		Menus.addSubMenuItem('sidebar', 'faqs', 'New Faq', 'faqs/create');
+		Menus.addMenuItem('sidebar', 'Preguntas frecuentes', 'faqs', 'dropdown', '/faqs(/create)?', true, null, null, 'icon-info');
+		/*Menus.addSubMenuItem('sidebar', 'faqs', 'List Faqs', 'faqs');*/
+		/*Menus.addSubMenuItem('sidebar', 'faqs', 'New Faq', 'faqs/create');*/
 	}
 ]);
