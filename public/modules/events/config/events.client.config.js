2b673cd96aa256ad8bd317e62ebdb7c165aa96df
H3rN4n <desouza030689@gmail.com>
2015-03-27 18:34:57 -0300

    beta updates

--- public/modules/events/config/events.client.config.js
+++ public/modules/events/config/events.client.config.js
@@ -6,6 +6,6 @@ angular.module('events').run(['Menus',
 		// Set top bar menu items
 		Menus.addMenuItem('sidebar', 'Eventos', 'events', 'dropdown', '/events(/create)?', false, null, null, 'fa fa-calendar');
 		/*Menus.addSubMenuItem('sidebar', 'events', 'List Events', 'events');*/
-		Menus.addSubMenuItem('sidebar', 'events', 'Nuevo Evento', 'events/create', false);
+		/*Menus.addSubMenuItem('sidebar', 'events', 'Nuevo Evento', 'events/create', false);*/
 	}
 ]);
