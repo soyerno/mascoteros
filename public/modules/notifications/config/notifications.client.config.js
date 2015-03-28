909aff9eb2ab03bf344e1e2b8004dcbc493fe9c8
H3rN4n <desouza030689@gmail.com>
2015-03-28 20:11:55 -0300

    notifications

--- public/modules/notifications/config/notifications.client.config.js
+++ public/modules/notifications/config/notifications.client.config.js
@@ -1,11 +1,12 @@
+/*
 'use strict';
 
 // Configuring the Articles module
 angular.module('notifications').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('sidebar', 'Alertas', 'notifications', 'dropdown', '/notifications(/create)?');
-		/*Menus.addSubMenuItem('sidebar', 'notifications', 'List Notifications', 'notifications');*/
-		/*Menus.addSubMenuItem('sidebar', 'notifications', 'New Notification', 'notifications/create');*/
+		Menus.addMenuItem('sidebar', 'Notifications', 'notifications', 'dropdown', '/notifications(/create)?');
+		Menus.addSubMenuItem('sidebar', 'notifications', 'List Notifications', 'notifications');
+		Menus.addSubMenuItem('sidebar', 'notifications', 'New Notification', 'notifications/create');
 	}
-]);
+]);*/
