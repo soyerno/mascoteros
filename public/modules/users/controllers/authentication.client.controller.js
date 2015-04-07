213559affe26f2adfafd1c3b266c4ecfbd8ae3b2
H3rN4n <desouza030689@gmail.com>
2015-04-07 11:38:20 -0300

    authentication success routes updated

--- public/modules/users/controllers/authentication.client.controller.js
+++ public/modules/users/controllers/authentication.client.controller.js
@@ -13,7 +13,7 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 				$scope.authentication.user = response;
 
 				// And redirect to the index page
-				$location.path('/');
+				$location.path('/pets');
 			}).error(function(response) {
 				$scope.error = response.message;
 			});
