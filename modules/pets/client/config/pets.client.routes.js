09dcafa6d3d4cdd140dedc466bcec7e6ba8cfcd6
H3rN4n <desouza030689@gmail.com>
2015-04-12 17:11:44 -0300

    mergin pet modules

--- modules/pets/client/config/pets.client.routes.js
+++ modules/pets/client/config/pets.client.routes.js
@@ -25,6 +25,48 @@ angular.module('pets').config(['$stateProvider',
 		state('pets.edit', {
 			url: '/:petId/edit',
 			templateUrl: 'modules/pets/views/edit-pet.client.view.html'
+		}).
+		state('petsgenres', {
+			abstract: true,
+			url: '/petsgenres',
+			template: '<ui-view/>'
+		}).
+		state('list.petgenres', {
+			url: '',
+			templateUrl: 'modules/pets/views/list-petgenres.client.view.html'
+		}).
+		state('petsgenres.create', {
+			url: '/create',
+			templateUrl: 'modules/pets/views/create-petgenre.client.view.html'
+		}).
+		state('petsgenres.view', {
+			url: '/:petgenreId',
+			templateUrl: 'modules/pets/views/view-petgenre.client.view.html'
+		}).
+		state('petsgenres.edit', {
+			url: '/:petgenreId/edit',
+			templateUrl: 'modules/pets/views/edit-petgenre.client.view.html'
+		}).
+		state('pettype', {
+			abstract: true,
+			url: '/petstypes',
+			template: '<ui-view/>'
+		}).
+		state('list.pettypes', {
+			url: '',
+			templateUrl: 'modules/pets/views/list-pettypes.client.view.html'
+		}).
+		state('pettypes.create', {
+			url: '/create',
+			templateUrl: 'modules/pets/views/create-pettype.client.view.html'
+		}).
+		state('pettypes.view', {
+			url: '/:pettypeId',
+			templateUrl: 'modules/pets/views/view-pettype.client.view.html'
+		}).
+		state('pettypes.edit', {
+			url: '/:pettypeId/edit',
+			templateUrl: 'modules/pets/views/edit-pettype.client.view.html'
 		});
 	}
 ]);
