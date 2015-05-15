fb3372a8496b756856658127a571e8f246a8775b
H3rN4n <desouza030689@gmail.com>
2015-05-15 12:50:03 -0300

    fb route

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -11,6 +11,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 		state('home', {
 			url: '/',
 			templateUrl: 'modules/core/views/home.client.view.html'
+		}).
+		state('homefb', {
+			url: '/facebook/',
+			templateUrl: 'modules/core/views/home.client.view.html'
 		});
 	}
 ]);
