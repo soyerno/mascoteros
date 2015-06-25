235ddf6d772fd35d97aa6267f4a1ad37701e2eae
H3rN4n <desouza030689@gmail.com>
2015-06-25 09:14:46 -0300

    update production assets list

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,17 +4,39 @@ module.exports = {
 	client: {
 		lib: {
 			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/Bootflat/css/bootstrap.min.css',
+				//'public/lib/Bootflat/bootflat/css/bootflat.min.css',
+				//'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/angular-datepicker/dist/index.css',
+				'public/lib/angular-socialshare/angular-socialshare.css',
+				'public/lib/textAngular/src/textAngular.css',
+				'public/lib/font-awesome/css/font-awesome.css',
+				'public/lib/animate.css/animate.min.css'
 			],
 			js: [
+				'public/lib/lodash/lodash.min.js',
 				'public/lib/angular/angular.min.js',
+				'public/lib/wowjs/dist/wow.min.js',
 				'public/lib/angular-resource/angular-resource.min.js',
 				'public/lib/angular-animate/angular-animate.min.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-				'public/lib/angular-file-upload/angular-file-upload.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-file-upload/angular-file-upload.js',
+				'public/lib/restangular/dist/restangular.js',
+				'public/lib/angularjs-geolocation/dist/angularjs-geolocation.min.js',
+				'public/lib/angular-google-maps/dist/angular-google-maps.js',
+				'public/lib/angular-datepicker/dist/index.js',
+				'public/lib/ngstorage/ngStorage.js',
+				'public/lib/textAngular/dist/textAngular.min.js',
+				'public/lib/textAngular/dist/textAngular-rangy.min.js',
+				'public/lib/textAngular/dist/textAngular-sanitize.min.js',
+				'public/lib/moment/moment.js',
+				'public/lib/angular-moment/angular-moment.js',
+				'public/lib/moment/locale/es.js',
+				'http://platform.twitter.com/widgets.js',
+				'public/lib/angular-socialshare/angular-socialshare.js',
+				'public/lib/angular-bootstrap-checkbox-fa/angular-bootstrap-checkbox.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
